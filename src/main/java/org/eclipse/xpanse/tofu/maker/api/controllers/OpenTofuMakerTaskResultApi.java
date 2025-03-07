38a1d7f5b16f6bab3f15182ace276f108150b91a
baixinsui <baixinsui@outlook.com>
2025-03-07 10:19:40 +0800

    Update reFetch result of order

--- src/main/java/org/eclipse/xpanse/tofu/maker/api/controllers/OpenTofuMakerTaskResultApi.java
+++ src/main/java/org/eclipse/xpanse/tofu/maker/api/controllers/OpenTofuMakerTaskResultApi.java
@@ -11,16 +11,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.xpanse.tofu.maker.models.response.OpenTofuResult;
 import org.eclipse.xpanse.tofu.maker.models.response.ReFetchResult;
-import org.eclipse.xpanse.tofu.maker.models.response.ReFetchState;
 import org.eclipse.xpanse.tofu.maker.opentofu.service.OpenTofuResultPersistenceManage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -55,7 +51,7 @@ public class OpenTofuMakerTaskResultApi {
                             + "receives a failure while sending the openTofu result via callback.")
     @GetMapping(value = "/result/{requestId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<OpenTofuResult> getStoredTaskResultByRequestId(
+    public ReFetchResult getStoredTaskResultByRequestId(
             @Parameter(name = "requestId", description = "id of the request")
                     @PathVariable("requestId")
                     UUID requestId) {
@@ -74,40 +70,17 @@ public class OpenTofuMakerTaskResultApi {
     @Operation(description = "Method to batch retrieve stored openTofu results from tofu-maker.")
     @PostMapping(value = "/results/batch", consumes = MediaType.APPLICATION_JSON_VALUE)
     public List<ReFetchResult> getBatchTaskResults(
-            @Parameter(description = "List of request IDs") @RequestBody List<UUID> requestIds) {
+            @Parameter(description = "List of request IDs", required = true) @RequestBody
+                    List<UUID> requestIds) {
         if (CollectionUtils.isEmpty(requestIds)) {
             throw new IllegalArgumentException("requestIds cannot be empty.");
         }
         List<ReFetchResult> reFetchResults = new ArrayList<>();
         requestIds.forEach(
                 requestId -> {
-                    try {
-                        ResponseEntity<OpenTofuResult> result =
-                                openTofuResultPersistenceManage.retrieveOpenTofuResultByRequestId(
-                                        requestId);
-                        if (result.getStatusCode() == HttpStatus.OK
-                                && Objects.nonNull(result.getBody())) {
-                            reFetchResults.add(
-                                    ReFetchResult.builder()
-                                            .openTofuResult(result.getBody())
-                                            .state(ReFetchState.OK)
-                                            .build());
-                        } else {
-                            reFetchResults.add(
-                                    ReFetchResult.builder()
-                                            .state(
-                                                    ReFetchState
-                                                            .RESULT_ALREADY_RETURNED_OR_REQUEST_ID_INVALID)
-                                            .build());
-                        }
-                    } catch (Exception e) {
-                        reFetchResults.add(
-                                ReFetchResult.builder()
-                                        .state(
-                                                ReFetchState
-                                                        .RESULT_ALREADY_RETURNED_OR_REQUEST_ID_INVALID)
-                                        .build());
-                    }
+                    reFetchResults.add(
+                            openTofuResultPersistenceManage.retrieveOpenTofuResultByRequestId(
+                                    requestId));
                 });
         return reFetchResults;
     }
